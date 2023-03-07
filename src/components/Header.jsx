470431fe87643624cbb71e0e41fa7ec18f6de7c5
Adrian Guerrero <adrianson@outlook.com>
2023-03-07 17:25:52 -0600

    add font and shadow text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,29 @@ import React from 'react'
 import styles from './Header.module.css'
 import logo from '../assets/Rectangle1.svg'
 import SearchBar from './SearchBar'
+import { Helmet } from 'react-helmet';
+
 
 export const Header = () => {
   return (
+
     <div className={styles.header}>
+      <Helmet>
+  <link
+    rel="preconnect"
+    href="https://fonts.googleapis.com"
+  />
+  <link
+    rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossorigin
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200;400;700&display=swap"
+    rel="stylesheet"
+  />
+</Helmet>
+
         <div className={styles.principal}>
             <img src={logo} alt="Logo App" />
             <SearchBar/>
